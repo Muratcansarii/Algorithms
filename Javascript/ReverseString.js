4ef0bd5710e4002137ec13bcd9c82a56425b3c2f
Muratcan Sarı <64330835+Muratcansarii@users.noreply.github.com>
2023-05-18 10:30:45 +0300

    commit message

---
+++ Javascript/ReverseString.js
@@ -0,0 +1,19 @@
+
+function ReverseStringIterative(string) {
+  if (typeof string !== 'string') {
+    throw new TypeError('The given value is not a string')
+  }
+  let reversedString = ''
+  let index
+
+  for (index = string.length - 1; index >= 0; index--) {
+    reversedString += string[index]
+  }
+
+  return reversedString
+}
+
+
+const ReverseStringIterativeInplace = (str) => [...str].reverse().join('')
+
+export { ReverseStringIterative, ReverseStringIterativeInplace }
