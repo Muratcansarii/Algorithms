59f4d97e6ea150732bf129d97b4f208a59dd445c
Muratcan Sarı <64330835+Muratcansarii@users.noreply.github.com>
2023-06-05 10:45:09 +0300

    commit message

---
+++ Javascript/CheckWordOccurrence.js
@@ -0,0 +1,21 @@
+
+const checkWordOccurrence = (str, isCaseSensitive = false) => {
+  if (typeof str !== 'string') {
+    throw new TypeError('The first param should be a string')
+  }
+
+  if (typeof isCaseSensitive !== 'boolean') {
+    throw new TypeError('The second param should be a boolean')
+  }
+
+  const modifiedStr = isCaseSensitive ? str.toLowerCase() : str
+
+  return modifiedStr
+    .split(/\s+/) 
+    .reduce((occurrence, word) => {
+      occurrence[word] = occurrence[word] + 1 || 1
+      return occurrence
+    }, {})
+}
+
+export { checkWordOccurrence }
