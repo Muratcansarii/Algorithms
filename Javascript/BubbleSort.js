80f2de85e02c83971743f751690ee55245d52f5c
Muratcan Sarı <64330835+Muratcansarii@users.noreply.github.com>
2023-07-02 10:58:29 +0300

    commit message

---
+++ Javascript/BubbleSort.js
@@ -0,0 +1,41 @@
+
+export function bubbleSort(items) {
+  const length = items.length
+  let noSwaps
+
+  for (let i = length; i > 0; i--) {
+    // flag for optimization
+    noSwaps = true
+    // Number of passes
+    for (let j = 0; j < i - 1; j++) {
+      // Compare the adjacent positions
+      if (items[j] > items[j + 1]) {
+        // Swap the numbers
+        ;[items[j], items[j + 1]] = [items[j + 1], items[j]]
+        noSwaps = false
+      }
+    }
+    if (noSwaps) {
+      break
+    }
+  }
+
+  return items
+}
+
+
+export function alternativeBubbleSort(arr) {
+  let swapped = true
+
+  while (swapped) {
+    swapped = false
+    for (let i = 0; i < arr.length - 1; i++) {
+      if (arr[i] > arr[i + 1]) {
+        ;[arr[i], arr[i + 1]] = [arr[i + 1], arr[i]]
+        swapped = true
+      }
+    }
+  }
+
+  return arr
+}
