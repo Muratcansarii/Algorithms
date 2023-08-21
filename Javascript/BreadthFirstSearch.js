5eca4c64506ac68ad7d88e6b66e87dce994e5da2
Muratcan Sarı <64330835+Muratcansarii@users.noreply.github.com>
2023-08-21 11:52:16 +0300

    commit message

---
+++ Javascript/BreadthFirstSearch.js
@@ -0,0 +1,29 @@
+import Queue from '../Data-Structures/Queue/Queue'
+
+
+export function breadthFirstSearch(graph, startingNode) {
+  // visited keeps track of all nodes visited
+  const visited = new Set()
+
+  // queue contains the nodes to be explored in the future
+  const queue = new Queue()
+  queue.enqueue(startingNode)
+
+  while (!queue.isEmpty()) {
+    // start with the queue's first node
+    const node = queue.dequeue()
+
+    if (!visited.has(node)) {
+      // mark the node as visited
+      visited.add(node)
+      const neighbors = graph[node]
+
+      // put all its neighbors into the queue
+      for (let i = 0; i < neighbors.length; i++) {
+        queue.enqueue(neighbors[i])
+      }
+    }
+  }
+
+  return visited
+}
