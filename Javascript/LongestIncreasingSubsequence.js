fec9d9199fe791fb35284b0c58d41ae9bce5ef95
Muratcan Sarı <64330835+Muratcansarii@users.noreply.github.com>
2023-09-07 12:13:59 +0300

    commit message

---
+++ Javascript/LongestIncreasingSubsequence.js
@@ -0,0 +1,22 @@
+
+function longestIncreasingSubsequence(x) {
+  const length = x.length
+  const dp = Array(length).fill(1)
+
+  let res = 1
+
+  for (let i = 0; i < length; i++) {
+    for (let j = 0; j < i; j++) {
+      if (x[i] > x[j]) {
+        dp[i] = Math.max(dp[i], 1 + dp[j])
+        if (dp[i] > res) {
+          res = dp[i]
+        }
+      }
+    }
+  }
+
+  return res
+}
+
+export { longestIncreasingSubsequence }
